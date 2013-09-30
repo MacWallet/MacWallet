edd1c0d88dd02cb090c50920215ff0271a894c26
Jonas Schnelli <jonas.schnelli@include7.ch>
2013-09-30 20:33:00 +0200

    updated sendCoins UI
    Signed-off-by: Jonas Schnelli <jonas.schnelli@include7.ch>

---
+++ btcee/btcee/BATransactionsWindowController.h
@@ -0,0 +1,13 @@
+//
+//  BATransactionsWindowController.h
+//  btcee
+//
+//  Created by Jonas Schnelli on 30.09.13.
+//  Copyright (c) 2013 include7 AG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BATransactionsWindowController : NSWindowController
+
+@end
---
+++ btcee/btcee/BATransactionsWindowController.m
@@ -0,0 +1,33 @@
+//
+//  BATransactionsWindowController.m
+//  btcee
+//
+//  Created by Jonas Schnelli on 30.09.13.
+//  Copyright (c) 2013 include7 AG. All rights reserved.
+//
+
+#import "BATransactionsWindowController.h"
+
+@interface BATransactionsWindowController ()
+
+@end
+
+@implementation BATransactionsWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
